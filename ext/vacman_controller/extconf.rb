07b567de6a49d7854b5a839ead043c10410a7d35
Ping Yu <ping@intridea.com>
2014-04-25 08:07:19 -0500

    search for installed lib and detected installed version

--- ext/vacman_controller/extconf.rb
+++ ext/vacman_controller/extconf.rb
@@ -1,8 +1,15 @@
 require 'mkmf'
 
-if find_library('aal2sdk-3.11.2', 'AAL2DPXInit', '/opt/vasco/VACMAN_Controller-3.11.2/lib')
-  create_makefile('vacman_controller/vacman_controller')
- else
+paths = Dir.glob('/opt/vasco/VACMAN_Controller-*').sort.reverse
+found = false
+if path.any? && version = paths.first().(/VACMAN_Controller\-(.*)$/)[1]
+  if find_library("aal2sdk-#{version}", 'AAL2DPXInit', "/opt/vasco/VACMAN_Controller-#{version}/lib")
+    found = true
+    create_makefile('vacman_controller/vacman_controller')
+  end
+end
+
+unless found
   puts "No libaal2sdk found"
   exit 1
 end
